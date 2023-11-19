94196cfa8020005604d426aebde50004bc62f0cc
Tran Trung Thanh <umbalaconmeogia@gmail.com>
2023-11-20 02:04:54 +0900

    Change to yii2-advanced framework.

---
+++ src/app/common/config/main.php
@@ -0,0 +1,18 @@
+<?php
+return [
+    'aliases' => [
+        '@bower' => '@vendor/bower-asset',
+        '@npm'   => '@vendor/npm-asset',
+    ],
+    'vendorPath' => dirname(dirname(__DIR__)) . '/../yii2/vendor',
+    'components' => [
+        'cache' => [
+            'class' => \yii\caching\FileCache::class,
+        ],
+    ],
+    'modules' => [
+        'systemuser' => [
+            'class' => 'umbalaconmeogia\systemuser\Module',
+        ],
+    ],
+];
